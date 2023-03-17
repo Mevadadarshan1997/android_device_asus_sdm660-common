9efa3c4b66695056ccad9c763a46daa77b262843
mevadadarshan19 <mevadadarshan19@gmail.com>
2023-03-17 20:28:51 +0530

    sdm660-common:revet sdm660-common:TARGET_KERNEL_CLANG_VERSION := r450784d

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -71,8 +71,13 @@ BOARD_KERNEL_IMAGE_NAME  := Image.gz-dtb
 TARGET_KERNEL_SOURCE := kernel/asus/sdm660
 
 ifeq ($(CUSTOM_CLANG),true)
+<<<<<<< HEAD
 TARGET_KERNEL_CLANG_VERSION := r450784d
 TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-r450784d
+=======
+TARGET_KERNEL_CLANG_VERSION := cosmic
+TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-cosmic
+>>>>>>> parent of af6b2c6 (sdm660-common : change to TARGET_KERNEL_CLANG_VERSION := r450784d)
 TARGET_KERNEL_ADDITIONAL_FLAGS := AR=llvm-ar AS=llvm-as NM=llvm-nm LD=ld.lld OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump OBJSIZE=llvm-size READELF=llvm-readelf STRIP=llvm-strip HOSTAR=llvm-ar HOSTAS=llvm-as HOSTNM=llvm-nm HOSTLD=ld.lld
 endif
 
